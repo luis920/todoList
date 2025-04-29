1afdf30065ce4ebb8df241c359f7ac4a340ba935
Luis Galvan <luis_galvan221@hotmail.com>
2025-04-29 13:08:57 -0600

    se agrego boton de cerrar sesion

--- src/Frontend/paginas/Task.jsx
+++ src/Frontend/paginas/Task.jsx
@@ -1,6 +1,12 @@
 import logo from "../imagenes/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlus,
+  faPencil,
+  faTrash,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import "../estilos/Task.css";
 
 const Task = () => {
   const Tareas = [
@@ -13,60 +19,72 @@ const Task = () => {
     },
   ];
   return (
-    <div className="d-flex justify-content-center flex-column align-items-center mt-3">
-      <img src={logo} alt="logo" className="imgLogo    " />
-      <div className="mb-4">
-        <button className="btn btn-primary mt-3">
+    <>
+      <div className=" d-flex justify-content-end mt-3 mx-3">
+        <button className="btn btn-danger mt-3">
           <FontAwesomeIcon
             className="icon-sidebar text-light mx-1"
-            icon={faPlus}
+            icon={faRightFromBracket}
           />
-          Agregar Tarea
+          Cerrar Sesion
         </button>
+      </div>
 
-        {/* Tabla de Tareas */}
-        <div className="table-responsive">
-          <h1 className="">Historial de tareas</h1>
-          <table className="table table-bordered bg-light">
-            <thead className="table-dark ">
-              <tr>
-                <th>Tarea ID</th>
-                <th>Titulo</th>
-                <th>Descripcion</th>
-                <th>Estado de la tarea</th>
-                <th>Id del usuario asignado</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Tareas.map((tarea) => (
-                <tr key={tarea.Id}>
-                  <td>{tarea.Id}</td>
-                  <td>{tarea.Titulo}</td>
-                  <td>{tarea.Descripcion}</td>
-                  <td>{tarea.Estado}</td>
-                  <td>{tarea.Usuario_Id}</td>
-                  <td>
-                    <button className="Btn">
-                      <FontAwesomeIcon
-                        className="icon-actions-pen"
-                        icon={faPencil}
-                      />
-                    </button>
-                    <button className="Btn">
-                      <FontAwesomeIcon
-                        className="icon-actions-trash"
-                        icon={faTrash}
-                      />
-                    </button>
-                  </td>
+      <div className="d-flex justify-content-center flex-column align-items-center mt-3">
+        <img src={logo} alt="logo" className="imgLogo    " />
+        <div className="mb-4">
+          <button className="btn btn-primary mt-3">
+            <FontAwesomeIcon
+              className="icon-sidebar text-light mx-1"
+              icon={faPlus}
+            />
+            Agregar Tarea
+          </button>
+
+          {/* Tabla de Tareas */}
+          <div className="table-responsive">
+            <h1 className="">Historial de tareas</h1>
+            <table className="table table-bordered bg-light">
+              <thead className="table-dark ">
+                <tr>
+                  <th>Tarea ID</th>
+                  <th>Titulo</th>
+                  <th>Descripcion</th>
+                  <th>Estado de la tarea</th>
+                  <th>Id del usuario asignado</th>
+                  <th>Acciones</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {Tareas.map((tarea) => (
+                  <tr key={tarea.Id}>
+                    <td>{tarea.Id}</td>
+                    <td>{tarea.Titulo}</td>
+                    <td>{tarea.Descripcion}</td>
+                    <td>{tarea.Estado}</td>
+                    <td>{tarea.Usuario_Id}</td>
+                    <td>
+                      <button>
+                        <FontAwesomeIcon
+                          className="icon-actions-pen"
+                          icon={faPencil}
+                        />
+                      </button>
+                      <button>
+                        <FontAwesomeIcon
+                          className="icon-actions-trash"
+                          icon={faTrash}
+                        />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Task;
